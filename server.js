cca62c5a4b75935eddf2b6f2f7c03187cbbabf5d
Keren Shimoni <kerenshimoni6@gmail.com>
2018-06-01 18:41:25 -0700

    server done, models done, routing started, handlebars started

--- server.js
+++ server.js
@@ -24,10 +24,84 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
-
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 //GET ROUTES
 //POST ROUTES 
+
+// Main route (test)
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+// A GET route for scraping 
+app.get("/scrape", function(req, res) {
+// Making a request for allrecipes, healthy category. The page's HTML is passed as the callback's third argument
+request("https://www.allrecipes.com/recipes/84/healthy-recipes/", function(error, response, html) {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  if (error){
+    return res.json(error);
+  }
+  
+  var $ = cheerio.load(html);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+
+  // With cheerio, find each h3 with the "fixed-recipe-card__h3" class
+  // (i: iterator. element: the current element)
+  $("img.fixed-recipe-card__img").each(function(i, element) {
+
+    // Save the text of the element in a "title" variable
+    var title = $(element).attr("title");
+
+    var imgURL = $(element).attr("data-original-src");
+    // In the currently selected element, look at its child elements (i.e., its a-tags),
+    // then save the values for any "href" attributes that the child elements may have
+    var link = $(element).parent().attr("href");
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+    if (title && imgURL && link){
+      results.push({
+        title: title,
+        imgURL: imgURL,
+        link: link
+      });
+    }
+  });
+
+  // Create a new Article using the `result` object built from scraping
+  db.Article.create(results)
+    .then(function(dbArticle) {
+      return res.status(200).send();
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      return res.json(err);
+    });
+  });
+});
+
+//get all articles from db 
+app.get("/articles", (req, res) => {
+  db.Article.find({}).then(function (dbArticle) {
+      // res.json(dbArticle);
+      res.send(dbArticle);
+  })
+      .catch(function (err) {
+          res.json(err);
+      });
+});
+
+
+//save article route //update... save=true 
+//delete article route
+//post comment route
+//delete comment route 
+
 
 // Start the server
 app.listen(PORT, function() {
